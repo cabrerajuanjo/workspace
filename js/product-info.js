58425af1e31cc3d3e48525e9ce118e402c9685c2
Juan Jose Cabrera <juanjo@JJ-debian>
2020-09-11 03:01:05 -0300

    Ob3, impresión de productos

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,57 @@
+var product = {};
+var products = {};
+var relatedProducts = [];
+
+function showImagesGallery(array, locationId){
+
+    let htmlContentToAppend = "";
+    for(var i = 0; i < array.length; i++){
+        let imageSrc = array[i];
+        htmlContentToAppend += `
+        <div class="col-lg-3 col-md-4 col-6">
+            <div class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
+            </div>
+        </div>
+        `
+    }
+    document.getElementById(locationId).innerHTML = htmlContentToAppend;
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            product = resultObj.data;
 
+            let productNameHTML  = document.getElementById("productName");
+            let productDescriptionHTML = document.getElementById("productDescription");
+            let productCostAndCurrencyHTML = document.getElementById("productCostAndCurrency");
+            let productSoldCountHTML = document.getElementById("productCount");
+
+            productNameHTML.innerHTML = product.name;
+            productDescriptionHTML.innerHTML = product.description;
+            productCostAndCurrencyHTML.innerHTML = product.currency + ' ' + product.cost;
+            productSoldCountHTML.innerHTML = product.soldCount;
+
+            //Muestro las imagenes en forma de galería
+            showImagesGallery(product.images, "productImagesGallery");
+
+            getJSONData(PRODUCTS_URL).then(function(resultObj){
+                if (resultObj.status === "ok")
+                {
+                    products = resultObj.data;
+                    for (var i = 0; i < product.relatedProducts.length; i++)
+                    {
+                        relatedProducts[i] = products[product.relatedProducts[i]].imgSrc;
+                    }
+
+                    showImagesGallery(relatedProducts, "relatedProducts");
+                }
+            });
+        }
+    });
 });
--- js/products.js
+++ js/products.js
@@ -40,9 +40,7 @@ function sortProducts(criteria, array){
     return result;
 }
 
-
 function showProductsList(array){
-
     let htmlContentToAppend = "";
     for(let i = 0; i < array.length; i++){
         let product = array[i];
@@ -51,32 +49,34 @@ function showProductsList(array){
             ((maxCost == undefined) || (maxCost != undefined && parseInt(product.cost) <= maxCost)))
         {
             htmlContentToAppend += `
-            <div class="list-group-item list-group-item-action">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="row">
-                            <div class="d-flex w-100 justify-content-between">
-                                <h4 class="mb-1">`+ product.name +`</h4>
-                                <small class="text-muted">` + product.soldCount + ` vendidos</small>
+            <a href="product-info.html?producto=` + i + `" class="list-group-item-action">
+                <div class="list-group-item list-group-item-action">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                        </div>
+                        <div class="col">
+                            <div class="row">
+                                <div class="d-flex w-100 justify-content-between">
+                                    <h4 class="mb-1">`+ product.name +`</h4>
+                                    <small class="text-muted">` + product.soldCount + ` vendidos</small>
+                                </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <h6>` + product.description + `</h6>
-                        </div>
-                        <div class="row">
-                            <h4><br></h4>
-                        </div>
-                        <div class="row_justified_price">
-                            <h5>` + product.currency + `</h5> &nbsp <h2>` + product.cost + `</h2>
-                        </div>
+                            <div class="row">
+                                <h6>` + product.description + `</h6>
+                            </div>
+                            <div class="row">
+                                <h4><br></h4>
+                            </div>
+                            <div class="row_justified_price">
+                                <h5>` + product.currency + `</h5> &nbsp <h2>` + product.cost + `</h2>
+                            </div>
 
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </a>
             `
         }
 
